c28646cd6eb10ccd437c2a2842e6cafabe6e74a2
IsabellaRina <74595066+IsabellaRina@users.noreply.github.com>
2022-02-18 11:05:52 +0000

    info, necklace, handbag, bracelets

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('necklace', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
+insert into products (name, description, code, price) values ('necklace', 'gold colored necklace with shell charms', '816905633-0', 10.5);
 insert into products (name, description, code, price) values ('gloves', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('bracelet', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('handbag', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+insert into products (name, description, code, price) values ('bracelet', 'silver colored bracelet with colorful beads', '445924201-X', 13.5);
+insert into products (name, description, code, price) values ('handbag', 'black leather handbag', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('buttons', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('earrings', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 insert into products (name, description, code, price) values ('ring', 'silver', '492662587-7', 10);
