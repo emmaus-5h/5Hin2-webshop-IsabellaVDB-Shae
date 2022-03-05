766fcbf9293fe63d2a1c9d44cdca84bd4f831d87
ShaeKidjo4HA <74540109+ShaeKidjo4HA@users.noreply.github.com>
2022-03-05 22:16:23 +0000

    beschrijving producten

--- db/create.sql
+++ db/create.sql
@@ -27,13 +27,13 @@ CREATE TABLE ratings (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, material, ratings_id) values ('necklace', 'gold colored necklace with shell charms', '816905633-0', 10.5, 'gold', 4);
-insert into products (name, description, code, price, material, ratings_id) values ('gloves', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11, 'nylon', 3);
+insert into products (name, description, code, price, material, ratings_id) values ('necklace', 'colorfull necklace with beads', '816905633-0', 3.5, 'gold', 3);
+insert into products (name, description, code, price, material, ratings_id) values ('gloves', 'leather gloves', '077030122-3', 18, 'leather', 3);
 insert into products (name, description, code, price, material, ratings_id) values ('bracelet', 'gold bracelet with silver beads', '445924201-X', 13.5, 'gold', 5);
-insert into products (name, description, code, price, material, ratings_id) values ('handbag', 'black leather handbag', '693155505-7', 13.5, 'leather', 5);
-insert into products (name, description, code, price, material) values ('buttons', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14, 'plastic');
-insert into products (name, description, code, price, material, ratings_id) values ('earrings', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 'aluminium', 3);
-insert into products (name, description, code, price, material, ratings_id) values ('ring', 'silver', '492662587-7', 10, 'silver', 4);
+insert into products (name, description, code, price, material, ratings_id) values ('handbag', 'black leather handbag', '693155505-7', 28, 'leather', 4);
+insert into products (name, description, code, price, material) values ('buttons', 'colorfull buttons to clip on your clothes', '686928463-6', 5.25, 'metal');
+insert into products (name, description, code, price, material, ratings_id) values ('earrings', 'hanging star earrings', '492662523-7', 12.5, 'gold', 5);
+insert into products (name, description, code, price, material, ratings_id) values ('ring', 'gold ring with flower print', '492662587-7', 7.5, 'gold', 4);
 
 insert into ratings (rating) values ('1/5');
 insert into ratings (rating) values ('2/5');
